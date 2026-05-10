30f5b3d727796493a31cf84fa83e1b7a776e580a
diegoalegil <diegogildam@gmail.com>
2026-05-10 22:03:20 +0100

    feat: migrate UI to FXML

--- src/main/java/io/github/diegoalegil/garageos/PrincipalApplication.java
+++ src/main/java/io/github/diegoalegil/garageos/PrincipalApplication.java
@@ -1,18 +1,19 @@
 package io.github.diegoalegil.garageos;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PrincipalApplication extends Application {
 
     @Override
-    public void start(Stage stage) {
-        Label saludo = new Label("¡Bienvenido a GarageOS!");
-        StackPane root = new StackPane(saludo);
-        Scene scene = new Scene(root, 400, 300);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                PrincipalApplication.class.getResource("principal-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
 
         stage.setTitle("GarageOS");
         stage.setScene(scene);
